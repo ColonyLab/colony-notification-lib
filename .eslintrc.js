f392ec2405d8b63fdaed0de0d4df15be98073d54
hbdgr <hbdgr@protonmail.com>
2024-04-18 16:44:36 +0200

    notification-lib: upgrade initial project configuration

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true, // if using React with browser environment
+    es2021: true,
+    mocha: true,
+    node: true,
+    jest: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "import"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,  // Enable this if you're using React
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:jest/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "comma-dangle": ["error", "always-multiline"],
+    "semi": ["error", "always"],
+    // "quotes": ["error", "double"],
+    "space-before-function-paren": [
+      "error", {
+        "anonymous": "always",
+        "named": "ignore",
+        "asyncArrow": "always",
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: "detect",  // Helps linting with correct React version
+    },
+  },
+};
